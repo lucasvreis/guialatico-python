df9b4d49b709d7ed38b8f86d57f95b120cdef3d1
Lucas V <lucasvianareis@gmail.com>
2019-05-06 22:38:23 -0300

    YES WORKING A LITTLE MORE DAMN

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import os
 os.environ['KIVY_SDL2_PATH'] = '~/lib'
 import kivy
 from kivy.app import App
-from common.brickfinder import BrickFinderWidget
-class InfoApp(App):
-    def build(self):
-        bfw = BrickFinderWidget(title='NXT Demo',start=True)
-        bfw.bind(on_search_complete=self.brickfound_callback)
-        return bfw
+from kivy.uix.button import Button
+from plyer import gps, gravity
+import importlib
+importlib.import_module('request-json')
 
-    def brickfound_callback(self, finder_widget, brick):
-        if brick:
-            print('we found it :-)')
-        else:
-            print('no brick :-(')
+class Guialatico(App):
+    gps_location = (0,0,0)
+    bearing = 0
+    def on_location(self, **kwargs):
+        self.gps_location = (kwargs['lat'], kwargs['lon'], kwargs['altitude'])
+        self.bearing = kwargs['bearing']
+
+    def build(self):
+        try:
+            gravity.enable()
+            compass.enable()
+            gps.configure(self.on_location, on_status=None)
+        except:
+            print("GPS, Compass or Gravity sensors not implemented")
+        return Button(text='hello world')
 
 
 if __name__ == '__main__':
-    InfoApp().run()
+    Guialatico().run()
--- request-json.py
+++ request-json.py
@@ -4,43 +4,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 api = ''
 
-# returns a lerp of azimuth and elevation in the interval [t+1, t+11]
-def getnext10seconds(lat, lng, alt):
-    url = 'https://www.n2yo.com/rest/v1/satellite/positions/25544/{}/{}/{}/10/&apiKey={}'.format(lat,lng,alt,'SBRY9Q-LVVSFA-L6KE5G-3ZSV')
+class GuialaticoData:
+    # returns a lerp of azimuth and elevation in the interval [t+1, t+11]
+    def getnext10seconds(self, lat, lon, alt):
+        url = 'https://www.n2yo.com/rest/v1/satellite/positions/25544/{}/{}/{}/10/&apiKey={}'.format(lat,lon,alt,'SBRY9Q-LVVSFA-L6KE5G-3ZSV')
 
 
-    resp = requests.get(url)
-    data = resp.json()
-    azimuths = [data['positions'][t]['azimuth'] for t in range(10)]
-    elevations = [data['positions'][t]['elevation'] for t in range(10)]
-    azimuth = lambda a: np.interp(a, range(10), azimuths)
-    elevation = lambda a: np.interp(a, range(10), elevations)
-    return (azimuth, elevation)
+        resp = requests.get(url)
+        data = resp.json()
+        azimuths = [data['positions'][t]['azimuth'] for t in range(10)]
+        elevations = [data['positions'][t]['elevation'] for t in range(10)]
+        azimuth = lambda a: np.interp(a, range(10), azimuths)
+        elevation = lambda a: np.interp(a, range(10), elevations)
+        return (azimuth, elevation)
 
-x = np.linspace(0, 10, 50)
-data = getnext10seconds(0,0,0)
-fig = plt.figure()
-ax1 = fig.add_subplot(211, projection='polar')
-ax2 = fig.add_subplot(212, projection='polar')
-# plt.ylim((0,90))
-plt.subplot(2,1,1)
-plt.title('Uphead')
-plt.ylim(-90.0,0)
-plt.plot(data[0](x), -data[1](x))
-plt.subplot(2,1,2)
-plt.title('Downhead')
-plt.ylim(-90.0,0)
-plt.plot(data[0](x), data[1](x))
+    def show(self):
+        x = np.linspace(0, 10, 50)
+        data = self.getnext10seconds(0,0,0)
+        fig = plt.figure()
+        ax1 = fig.add_subplot(211, projection='polar')
+        ax2 = fig.add_subplot(212, projection='polar')
+        # plt.ylim((0,90))
+        plt.subplot(2,1,1)
+        plt.title('Uphead')
+        plt.ylim(-90.0,0)
+        plt.plot(data[0](x), -data[1](x))
+        plt.subplot(2,1,2)
+        plt.title('Downhead')
+        plt.ylim(-90.0,0)
+        plt.plot(data[0](x), data[1](x))
 
-plt.show()
-
-# >>> x = np.linspace(0, 2*np.pi, 10)
-# >>> y = np.sin(x)
-# >>> xvals = np.linspace(0, 2*np.pi, 50)
-# >>> yinterp = np.interp(xvals, x, y)
-# >>> import matplotlib.pyplot as plt
-# >>> plt.plot(x, y, 'o')
-# [<matplotlib.lines.Line2D object at 0x...>]
-# >>> plt.plot(xvals, yinterp, '-x')
-# [<matplotlib.lines.Line2D object at 0x...>]
-# >>> plt.show()
+        plt.show()
